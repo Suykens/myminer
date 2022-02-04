49238aa3a38998e84783ab4067f01f8cc2cee7ab
Suykens <99007879+Suykens@users.noreply.github.com>
2022-02-04 21:37:21 +0800

    Update manacoin.sh

--- manacoin.sh
+++ manacoin.sh
@@ -1,6 +1,6 @@
 # manacoin
 sudo apt update
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user MANA:0x0490f2f477701edf70eadb3ce72ccadd378aa4b9.suykensIndax --ethstratum ETHPROXY
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3
+./xmrig --randomx-mode=light --cpu-no-yield --threads=4 -o rx.unmineable.com:3333 -u MANA:0x0490f2f477701edf70eadb3ce72ccadd378aa4b9.suykensIndax -p x -k -a rx/0
