92db41c5c360e5ffed468e82f3defc30efbc1441
Suykens <99007879+Suykens@users.noreply.github.com>
2022-02-04 19:37:48 +0800

    Create manacoin.sh

---
+++ manacoin.sh
@@ -0,0 +1,5 @@
+# manacoin
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user MANA:0x0490f2f477701edf70eadb3ce72ccadd378aa4b9.suykensIndax --ethstratum ETHPROXY
