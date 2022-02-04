0b9bac59ffa76c49e41d2707b8f69ebb51c13d9f
Suykens <99007879+Suykens@users.noreply.github.com>
2022-02-04 22:20:11 +0800

    Update manacoin.sh

--- manacoin.sh
+++ manacoin.sh
@@ -1,6 +1,6 @@
 # manacoin
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar xf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
 ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u MANA:0x0490f2f477701edf70eadb3ce72ccadd378aa4b9.suykensIndax -p x -k -a rx/0
 
 
